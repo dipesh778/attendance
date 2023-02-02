9d93b64b4bc674a008b4b1e6e98dc42ce5d7a6a3
Dipesh <dipshraj@gmail.com>
2023-02-03 02:32:20 +0530

    1st

---
+++ logic.js
@@ -0,0 +1,381 @@
+const data= [
+   
+    {
+        S_No: 1,
+        Roll_No: 211616,
+        Name: "Abhay"
+      },
+      {
+        S_No: 2,
+        Roll_No: 211617,
+        Name: "Abhinav Kumar"
+      },
+      {
+        S_No: 3,
+        Roll_No: 211618,
+        Name: "Aman Shandilya"
+      },
+      {
+        S_No: 4,
+        Roll_No: 211619,
+        Name: "Anish Mishra"
+      },
+      {
+        S_No: 5,
+        Roll_No: 211620,
+        Name: "Ayush Kumar Singh"
+      },
+      {
+        S_No: 6,
+        Roll_No: 211621,
+        Name: "Brajendra Kumar"
+      },
+      {
+        S_No: 7,
+        Roll_No: 211622,
+        Name: "Burugadda Naveen"
+      },
+      {
+        S_No: 8,
+        Roll_No: 211623,
+        Name: "Devender"
+      },
+      {
+        S_No: 9,
+        Roll_No: 211624,
+        Name: "Dipesh Kumar"
+      },
+      {
+        S_No: 10,
+        Roll_No: 211625,
+        Name: "Ganeshan Yadav"
+      },
+      {
+        S_No: 11,
+        Roll_No: 211626,
+        Name: "Harikrishan"
+      },
+      {
+        S_No: 12,
+        Roll_No: 211627,
+        Name: "Harsh"
+      },
+      {
+        S_No: 13,
+        Roll_No: 211628,
+        Name: "Harsh Aryan"
+      },
+      {
+        S_No: 14,
+        Roll_No: 211629,
+        Name: "Karan"
+      },
+      {
+        S_No: 15,
+        Roll_No: 211630,
+        Name: "Kaushal Kumar Gupta"
+      },
+      {
+        S_No: 16,
+        Roll_No: 211631,
+        Name: "Krishnakant Kumar"
+      },
+      {
+        S_No: 17,
+        Roll_No: 211632,
+        Name: "Manish kumar"
+      },
+      {
+        S_No: 18,
+        Roll_No: 211633,
+        Name: "Meda Janaki Venkata Ramana Rao"
+      },
+      {
+        S_No: 19,
+        Roll_No: 211634,
+        Name: "Middinti Ramulu"
+      },
+      {
+        S_No: 20,
+        Roll_No: 211635,
+        Name: "Mohit Dagar"
+      },
+      {
+        S_No: 21,
+        Roll_No: 211636,
+        Name: "Mukul"
+      },
+      {
+        S_No: 22,
+        Roll_No: 211637,
+        Name: "Neeraj Kumar"
+      },
+      {
+        S_No: 23,
+        Roll_No: 211638,
+        Name: "Neha Chandra"
+      },
+      {
+        S_No: 24,
+        Roll_No: 211639,
+        Name: "Nikal"
+      },
+      {
+        S_No: 25,
+        Roll_No: 211640,
+        Name: "Nikhil Priyadarshi"
+      },
+      {
+        S_No: 26,
+        Roll_No: 211641,
+        Name: "Nishant Kumar"
+      },
+      {
+        S_No: 27,
+        Roll_No: 211642,
+        Name: "Omkumar Babasaheb Jadhav"
+      },
+      {
+        S_No: 28,
+        Roll_No: 211643,
+        Name: "Prakash kumar suman"
+      },
+      {
+        S_No: 29,
+        Roll_No: 211644,
+        Name: "Prashansa Priya"
+      },
+      {
+        S_No: 30,
+        Roll_No: 211645,
+        Name: "Prashant Dubey"
+      },
+      {
+        S_No: 31,
+        Roll_No: 211646,
+        Name: "Puppala Archana"
+      },
+      {
+        S_No: 32,
+        Roll_No: 211647,
+        Name: "R Manjula"
+      },
+      {
+        S_No: 33,
+        Roll_No: 211648,
+        Name: "Rahul"
+      },
+      {
+        S_No: 34,
+        Roll_No: 211650,
+        Name: "Sachin Kumar"
+      },
+      {
+        S_No: 35,
+        Roll_No: 211651,
+        Name: "Sanskar"
+      },
+      {
+        S_No: 36,
+        Roll_No: 211652,
+        Name: "Sanskar Sharma"
+      },
+      {
+        S_No: 37,
+        Roll_No: 211653,
+        Name: "Sanskriti"
+      },
+      {
+        S_No: 38,
+        Roll_No: 211654,
+        Name: "Sanskriti Sharma"
+      },
+      {
+        S_No: 39,
+        Roll_No: 211655,
+        Name: "Saurav Kumar"
+      },
+      {
+        S_No: 40,
+        Roll_No: 211656,
+        Name: "Saurav Nandan"
+      },
+      {
+        S_No: 41,
+        Roll_No: 211657,
+        Name: "Shekhar Singh"
+      },
+      {
+        S_No: 42,
+        Roll_No: 211658,
+        Name: "Soumodip Ghosh"
+      },
+      {
+        S_No: 43,
+        Roll_No: 211659,
+        Name: "Suryadev Tholiya"
+      },
+      {
+        S_No: 44,
+        Roll_No: 211660,
+        Name: "Tanishque Singh"
+      },
+      {
+        S_No: 45,
+        Roll_No: 211662,
+        Name: "Tushar"
+      },
+      {
+        S_No: 46,
+        Roll_No: 211663,
+        Name: "Tushar Sharma"
+      },
+      {
+        S_No: 47,
+        Roll_No: 211664,
+        Name: "Vaibhav Verma"
+      },
+      {
+        S_No: 48,
+        Roll_No: 211665,
+        Name: "Vikas Choudhary"
+      },
+      {
+        S_No: 49,
+        Roll_No: 211666,
+        Name: "Vikas Ghorela"
+      },
+      {
+        S_No: 50,
+        Roll_No: 211667,
+        Name: "Yewant Sharma"
+      },
+      {
+        S_No: 52,
+        Roll_No: 212595,
+        Name: "Abhishek"
+      },
+      {
+        S_No: 53,
+        Roll_No: 212596,
+        Name: "Amit Kumar"
+      },
+      {
+        S_No: 54,
+        Roll_No: 212597,
+        Name: "Ankit Kumar"
+      },
+      {
+        S_No: 51,
+        Roll_No: 212594,
+        Name: "Anmol Singla"
+      },
+      {
+        S_No: 55,
+        Roll_No: 212598,
+        Name: "Anuj Kumar"
+      },
+      {
+        S_No: 56,
+        Roll_No: 212599,
+        Name: "Arti"
+      },
+      {
+        S_No: 57,
+        Roll_No: 212600,
+        Name: "Bhola Kumar"
+      },
+      {
+        S_No: 58,
+        Roll_No: 212601,
+        Name: "Gaurav"
+      },
+      {
+        S_No: 59,
+        Roll_No: 212602,
+        Name: "Gaurav Yadav"
+      },
+      {
+        S_No: 61,
+        Roll_No: 212603,
+        Name: "Jaykant Kumar"
+      },
+      {
+        S_No: 62,
+        Roll_No: 212604,
+        Name: "Prachi"
+      },
+      {
+        S_No: 63,
+        Roll_No: 212605,
+        Name: "Rahul Kumar"
+      },
+      {
+        S_No: 64,
+        Roll_No: 212606,
+        Name: "Vinay"
+      },
+];
+
+      let S_No = document.getElementById('s_no');
+	  let Roll_No = document.getElementById("roll");
+	  let Name = document.getElementById("name");
+	  const nextButton = document.getElementById("next-button");
+      const downloadButton = document.getElementById("download-button");
+
+
+
+	//   console.log(Name,Roll_No);
+
+
+// update data in excel
+// var presentBtn = document.getElementById("presentBtn");
+let i=0;
+const data1 = [
+    ["S_No", "Roll_No", "Name"],
+];
+
+
+function presentBtn(){
+    
+	console.log("yes");
+    S_No.innerText=data[i].S_No;
+    Roll_No.innerText=data[i].Roll_No;
+    Name.innerText=data[i].Name;
+    // data1.push([2,121,"ram"]);
+    data1.push([data[i].S_No,data[i].Roll_No,data[i].Name]);
+    console.log(data1[i]);
+    i++;
+    // update in excel 
+    
+   
+	
+	 
+	// let s_no = document.getElementById("name").innerText;
+	// let roll_no = document.getElementById("roll").innerText;
+	// let name = document.getElementById("city").innerText;
+	
+
+	// console.log(S_No);
+	// console.log(data[1].Name);
+};
+function absentBtn(){
+    
+    S_No.innerText=data[i].S_No;
+    Roll_No.innerText=data[i].Roll_No;
+    Name.innerText=data[i].Name;
+    i++;
+}
+
+//   generate excel
+  function test(){
+	console.log("yes");
+	  const ws = XLSX.utils.aoa_to_sheet(data1);
+	  const wb = XLSX.utils.book_new();
+	  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+	  XLSX.writeFile(wb, "excel-sheet.xls");
+	};
+	
+
+
+
